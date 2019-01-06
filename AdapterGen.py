c9879ba51ab1d12426f93f9d7be3f4478ce455a6
SunanthaSethuraman <s.sethuraman@wustl.edu>
2019-01-05 19:31:23 -0600

    removing AdapterGen.py

--- AdapterGen.py
+++
@@ -1,7 +0,0 @@
-import itertools
-
-keywords = [''.join(i) for i in itertools.product(['A','C','G','T'], repeat = 4)]
-new_keywords = [">Adapter_" + str(i) + "\n" + keywords[i]+ "TGGAATTCTCGGGTGCCAAGGC" for i in range(len(keywords))]
-
-with open("/diskmnt/Projects/Users/ssethura/cptac_mirna/FASTQ/Adapters.fa", 'w') as f:
-    f.write("\n".join(map(str, new_keywords)))
